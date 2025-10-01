b2a01bcbe18ba9cab804c75c4cd0f82d0864edf6
maryana sidorina <maryanasidorina9@gmail.com>
2025-10-01 13:05:15 +0300

    Delete sidorina_folder/завдання 1.js

--- sidorina_folder/завдання 1.js
+++
@@ -1,8 +0,0 @@
-let y = 0.3 * (Math.log(Math.E ** -2.3) / Math.log(5));
-let z = Math.sqrt(Math.abs(y + Math.sin(y) ** 2));
-let x = Math.acos((Math.PI - z) / 3) + Math.E;
-
-
-console.log("y =", y);
-console.log("z =", z);
-console.log("x =", x);
